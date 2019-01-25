fbf95214871de7c4a80945bced9b17f7c1f33da2
Ivan Mikhailov <erinithma@gmail.com>
2019-01-25 17:30:29 +0300

    add accord icon

--- dev/js/view/main.js
+++ dev/js/view/main.js
@@ -7,31 +7,37 @@ export default class Main extends React.Component{
             <div class="grid">
                 <div class="item">
                     <NavLink to="/тренажер/игра" className="item__content">
-                        <img src="/assets/img/play.svg" alt="Игра" width="128" height="128" />
+                        <img src="/assets/img/play.svg" alt="Игра" width="100" height="100" />
                         Игра
                     </NavLink>
                 </div>
                 <div class="item">
                     <NavLink to="/тренажер/игра" className="item__content">
-                        <img src="/assets/img/mindur.svg" alt="Мажор / минор" width="128" height="128" />
+                        <img src="/assets/img/mindur.svg" alt="Мажор / минор" width="100" height="100" />
                         Мажор / минор
                     </NavLink>
                 </div>
                 <div class="item">
-                    <a class="item__content" data-prevent href="ноты">
-                        <img src="/assets/img/note.svg" alt="Ноты" width="128" height="128" />
+                    <a class="item__content" href="ноты">
+                        <img src="/assets/img/note.svg" alt="Ноты" width="100" height="100" />
                         Ноты
                     </a>
                 </div>      
                 <div class="item">
-                    <a class="item__content" data-prevent href="интервалы">
-                        <img src="/assets/img/interval.svg" alt="Интервалы" width="128" height="128" />
+                    <a class="item__content" href="интервалы">
+                        <img src="/assets/img/interval.svg" alt="Интервалы" width="100" height="100" />
                         Интервалы
                     </a>
                 </div>    
                 <div class="item">
-                    <a class="item__content" data-prevent href="диктант">
-                        <img src="/assets/img/dictant.svg" alt="Диктант" width="128" height="128" />
+                    <a class="item__content" href="трезвучия">
+                        <img src="/assets/img/accord.svg" alt="Трезвучия" width="100" height="100" />
+                        Трезвучия
+                    </a>
+                </div>    
+                <div class="item">
+                    <a class="item__content" href="диктант">
+                        <img src="/assets/img/dictant.svg" alt="Диктант" width="100" height="100" />
                         Диктант
                     </a>     
                 </div>
